6a92b8a6738ac373f46fdae791f1f920f69c7d6e
hr9919 <hrnam99@chungbuk.ac.kr>
2026-03-22 02:10:51 +0900

    (Melting-Studio-Study#2) week02: BOJ 1436 영화감독 숌, BOJ 2750 수 정렬하기

---
+++ HAERIM_NAM/week02/boj_1436_영화감독 숌.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        int count = 0;
+        int num = 666;
+
+        while (true) {
+
+            if (String.valueOf(num).contains("666")) {
+                count++;
+            }
+
+            if (count == n) {
+                System.out.println(num);
+                break;
+            }
+
+            num++;
+        }
+    }
+}
