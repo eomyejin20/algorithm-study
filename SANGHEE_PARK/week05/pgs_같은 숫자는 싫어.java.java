acd60d461bc87c680d51086b3ae3d26eef4e5caf
Sanghee Park <hee165452@gmail.com>
2026-05-12 17:10:57 +0900

    (#17)week05: PGS 같은 숫자는 싫어, PGS 기능 개발

---
+++ SANGHEE_PARK/week05/pgs_같은 숫자는 싫어.java.java
@@ -0,0 +1,33 @@
+package week05;
+
+// pgs_같은 숫자는 싫어.java
+import java.util.*;
+
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // 첫 번째 숫자는 무조건 추가
+        list.add(arr[0]);
+
+        // 이전 숫자와 다를 때만 추가
+        for(int i = 1; i < arr.length; i++) {
+
+            if(arr[i] != arr[i - 1]) {
+                list.add(arr[i]);
+            }
+        }
+
+        // ArrayList -> int 배열 변환
+        int[] answer = new int[list.size()];
+
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+}
